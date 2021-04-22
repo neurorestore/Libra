aa9fb37740de88b6b6c247ccf0e6530afd789b85
Jordan Squair <jordansquair@gmail.com>
2021-04-22 11:49:09 +0200

    Add redundancy for Seurat V4

--- R/run_de.R
+++ R/run_de.R
@@ -1,37 +1,37 @@
 #' Run differential expression
-#' 
-#' Perform differential expression on single-cell data. Libra implements a total 
-#' of 22 unique differential expression methods that can all be accessed from 
-#' one function. These methods encompass traditional single-cell methods as well 
-#' as methods accounting for biological replicate including pseudobulk and 
-#' mixed model methods. The code for this package has been largely inspired 
-#' by the Seurat and Muscat packages. Please see the documentation of these 
+#'
+#' Perform differential expression on single-cell data. Libra implements a total
+#' of 22 unique differential expression methods that can all be accessed from
+#' one function. These methods encompass traditional single-cell methods as well
+#' as methods accounting for biological replicate including pseudobulk and
+#' mixed model methods. The code for this package has been largely inspired
+#' by the Seurat and Muscat packages. Please see the documentation of these
 #' packages for further information.
-#' 
+#'
 #' @param input a single-cell matrix to be converted, with features (genes) in rows
-#'   and cells in columns. Alternatively, a \code{Seurat}, \code{monocle3}, or 
+#'   and cells in columns. Alternatively, a \code{Seurat}, \code{monocle3}, or
 #'   or \code{SingleCellExperiment} object can be directly input.
 #' @param meta the accompanying meta data whereby the rownames match the column
-#'   names of \code{input}. 
-#' @param replicate_col the vector in \code{meta} containing the replicate 
+#'   names of \code{input}.
+#' @param replicate_col the vector in \code{meta} containing the replicate
 #'   information. Defaults to \code{replicate}.
-#' @param cell_type_col the vector in \code{meta} containing the cell type 
+#' @param cell_type_col the vector in \code{meta} containing the cell type
 #'   information. Defaults to \code{cell_type}.
 #' @param label_col the vector in \code{meta} containing the experimental
-#'   label. Defaults to \code{label}. 
+#'   label. Defaults to \code{label}.
 #' @param min_cells the minimum number of cells in a cell type to retain it.
 #'   Defaults to \code{3}.
 #' @param min_reps the minimum number of replicates in a cell type to retain it.
 #'   Defaults to \code{2}.
 #' @param min_features the minimum number of counts for a gene to retain it.
-#'   Defaults to \code{0}   
+#'   Defaults to \code{0}
 #' @param de_family the differential expression family to use. Available options
 #' are:
 #' \itemize{
-#' \item{"singlecell"}: Uses traditionally methods implemented by Seurat to 
+#' \item{"singlecell"}: Uses traditionally methods implemented by Seurat to
 #' test for differentially expressed genes. These methods do not take biological
 #' replicate into account. For single cell methods there are \code{six} options
-#' for \code{de_method} that can be used, while no input for 
+#' for \code{de_method} that can be used, while no input for
 #' \code{de_test} is required:
 #' \itemize{
 #' \item{"wilcox"}: Wilcoxon Rank-Sum test. The default.
@@ -41,11 +41,11 @@
 #' \item{"LR"}: Logistic regression
 #' \item{"MAST"}: MAST (requires installation of the \code{MAST} package).
 #' }
-#' 
+#'
 #' \item{"pseudobulk"}: These methods first convert the single-cell expression
 #' matrix to a so-called 'pseudobulk' matrix by summing counts for each gene
-#' within biological replicates, and then performing differential expression 
-#' using bulk RNA-seq methods. For pseudobulk methods there are \code{six} 
+#' within biological replicates, and then performing differential expression
+#' using bulk RNA-seq methods. For pseudobulk methods there are \code{six}
 #' different methods that can be accessed by combinations of \code{de_method}
 #' and \code{de_type}. First specify \code{de_method} as one of the following:
 #' \itemize{
@@ -54,7 +54,7 @@
 #' \code{de_type} as either \code{"LRT"} or \code{"QLF"} as the null hypothesis
 #' testing approach. See http://www.bioconductor.org/packages/release/bioc/html/edgeR.html
 #' for further information. The default.
-#' \item{"DESeq2"}: The DESeq2 method according to 
+#' \item{"DESeq2"}: The DESeq2 method according to
 #' Love et al, Genome Biology, 2014. For this method please specify
 #' \code{de_type} as either \code{"LRT"} or \code{"Wald"} as the null hypothesis
 #' testing approach. See https://bioconductor.org/packages/release/bioc/html/DESeq2.html
@@ -63,23 +63,23 @@
 #' Ritchie et al, Nucleic Acids Research, 2015. For this method please specify
 #' \code{de_type} as either \code{"voom"} or \code{"trend"} as the precise
 #' normalization and null hypothesis testing approach within limma. See
-#' https://bioconductor.org/packages/release/bioc/html/limma.html for 
+#' https://bioconductor.org/packages/release/bioc/html/limma.html for
 #' further information.
 #' }
 #' \item{"mixedmodel"}: Mixed model methods also take biological replicate into
 #' account by modelling it as a random effect. Please note that these methods
-#' are generally extremely computationally intensive. For mixed model methods 
-#' there are \code{ten} different methods that can be accessed by 
-#' combinations of \code{de_method} and \code{de_type}. 
+#' are generally extremely computationally intensive. For mixed model methods
+#' there are \code{ten} different methods that can be accessed by
+#' combinations of \code{de_method} and \code{de_type}.
 #' First specify \code{de_method} as one of the following:
 #' \itemize{
-#' \item{"negbinom"}: Negative binomial generalized linear mixed model. 
+#' \item{"negbinom"}: Negative binomial generalized linear mixed model.
 #' The default.
 #' \item{"linear"}: Linear mixed model.
 #' \item{"poisson"}: Poisson generalized linear mixed model.
 #' \item{"negbinom_offset"}: Negative binomial generalized linear mixed model
 #' with an offset term to account for sequencing depth differences between cells.
-#' \item{"poisson_offset"}: Poisson generalized linear mixed model with an 
+#' \item{"poisson_offset"}: Poisson generalized linear mixed model with an
 #' offset term to account for sequencing depth differences between cells.
 #' }
 #' For each of these options the user has the option to use either a Wald or
@@ -90,40 +90,40 @@
 #' Please see the documentation under \code{de_family} for precise usage options,
 #' or see the documentation at https://github.com/neurorestore/Libra. This
 #' option will default to \code{wilcox} for \code{singlecell} methods, to
-#' \code{edgeR} for \code{pseudobulk} methods, and \code{negbinom} for 
+#' \code{edgeR} for \code{pseudobulk} methods, and \code{negbinom} for
 #' \code{mixedmodel} methods.
-#' @param de_test the specific mixed model test to use. Please see the 
+#' @param de_test the specific mixed model test to use. Please see the
 #' documentation under \code{de_family} for precise usage options,
 #' or see the documentation at https://github.com/neurorestore/Libra. This
 #' option defaults to \code{NULL} for \code{singlecell} methods, to \code{LRT}
-#' for \code{pseudobulk} and \code{mixedmodel} methods. 
+#' for \code{pseudobulk} and \code{mixedmodel} methods.
 #' @param n_threads number of threads to use for parallelization in mixed models.
-#' 
-#' @return a data frame containing differential expression results with the 
+#'
+#' @return a data frame containing differential expression results with the
 #' following columns:
 #' \itemize{
 #' \item{"cell type"}: The cell type DE tests were run on. By default Libra
 #' will run DE on all cell types present in the original meta data.
 #' \item{"gene"}: The gene being tested.
-#' \item{"avg_logFC"}: The average log fold change between conditions. The 
+#' \item{"avg_logFC"}: The average log fold change between conditions. The
 #' direction of the logFC can be controlled using factor levels of \code{label_col}
 #' whereby a positive logFC reflects higher expression in the first level of
 #' the factor, compared to the second.
 #' \item{"p_val"}: The p-value resulting from the null hypothesis test.
-#' \item{"p_val_adj"}: The adjusted p-value according to the Benjamini 
+#' \item{"p_val_adj"}: The adjusted p-value according to the Benjamini
 #' Hochberg method (FDR).
 #' \item{"de_family"}: The differential expression method family.
 #' \item{"de_method"}: The precise differential expression method.
 #' \item{"de_type"}: The differential expression method statistical testing type.
 #' }
-#' 
+#'
 #' @importFrom magrittr  %<>%
 #' @importFrom forcats fct_recode
 #' @importFrom dplyr group_by mutate select ungroup arrange
 #' @export
-#' 
-run_de = function(input, 
-                  meta = NULL, 
+#'
+run_de = function(input,
+                  meta = NULL,
                   replicate_col = 'replicate',
                   cell_type_col = 'cell_type',
                   label_col = 'label',
@@ -137,8 +137,8 @@ run_de = function(input,
   # run differential expression
   DE = switch(de_family,
               pseudobulk = pseudobulk_de(
-                input = input, 
-                meta = meta, 
+                input = input,
+                meta = meta,
                 replicate_col = replicate_col,
                 cell_type_col = cell_type_col,
                 label_col = label_col,
@@ -150,8 +150,8 @@ run_de = function(input,
                 de_type = de_type
               ),
               mixedmodel = mixedmodel_de(
-                input = input, 
-                meta = meta, 
+                input = input,
+                meta = meta,
                 replicate_col = replicate_col,
                 cell_type_col = cell_type_col,
                 label_col = label_col,
@@ -162,7 +162,7 @@ run_de = function(input,
                 n_threads = n_threads
               ),
               singlecell = singlecell_de(
-                input = input, 
+                input = input,
                 meta = meta,
                 cell_type_col = cell_type_col,
                 label_col = label_col,
@@ -170,7 +170,7 @@ run_de = function(input,
                 de_method = de_method
               )
   )
-  
+
   # clean up the output
   suppressWarnings(
     colnames(DE) %<>%
@@ -183,12 +183,13 @@ run_de = function(input,
                'p_val_adj' = 'adj.P.Val',      ## limma
                'p_val_adj' = 'FDR',            ## edgeER
                'avg_logFC' = 'log2FoldChange', ## DESEeq2
-               'avg_logFC' = 'logFC' ## limma/edgeR
+               'avg_logFC' = 'logFC', ## limma/edgeR
+               'avg_logFC' = 'avg_log2FC' # Seurat V4
     )
     ) %>%
     as.character()
-  
-  DE %<>% 
+
+  DE %<>%
     # calculate adjusted p values
     group_by(cell_type) %>%
     mutate(p_val_adj = p.adjust(p_val, method = 'BH')) %>%
@@ -196,16 +197,15 @@ run_de = function(input,
     mutate(gene = as.character(gene)) %>%
     # invert logFC to match Seurat level coding
     mutate(avg_logFC = avg_logFC * -1) %>%
-    dplyr::select(cell_type, 
-                  gene, 
-                  avg_logFC, 
+    dplyr::select(cell_type,
+                  gene,
+                  avg_logFC,
                   p_val,
                   p_val_adj,
-                  de_family, 
-                  de_method, 
+                  de_family,
+                  de_method,
                   de_type
     ) %>%
     ungroup() %>%
     arrange(cell_type, gene)
 }
-  
