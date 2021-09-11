dd8311786e91463e87cede8d3bb8ae774c741608
Jordan Squair <jordansquair@gmail.com>
2021-09-11 08:50:45 -0700

    Do not check for only 2 labels

--- R/check_inputs.R
+++ R/check_inputs.R
@@ -1,33 +1,33 @@
 #' Check inputs
-#' 
+#'
 #' Check inputs prior to running Libra functions
 #'
-#' 
+#'
 #' @param input a single-cell matrix to be converted, with features (genes) in rows
-#'   and cells in columns. Alternatively, a \code{Seurat}, \code{monocole3}, or 
+#'   and cells in columns. Alternatively, a \code{Seurat}, \code{monocole3}, or
 #'   or \code{SingleCellExperiment} object can be directly input.
 #' @param meta the accompanying meta data whereby the rownames match the column
-#'   names of \code{input}. 
-#' @param replicate_col the vector in \code{meta} containing the replicate 
+#'   names of \code{input}.
+#' @param replicate_col the vector in \code{meta} containing the replicate
 #'   information. Defaults to \code{replicate}.
-#' @param cell_type_col the vector in \code{meta} containing the cell type 
+#' @param cell_type_col the vector in \code{meta} containing the cell type
 #'   information. Defaults to \code{cell_type}.
 #' @param label_col the vector in \code{meta} containing the experimental
-#'   label. Defaults to \code{label}. 
+#'   label. Defaults to \code{label}.
 #' @param min_cells the minimum number of cells in a cell type to retain it.
 #'   Defaults to \code{3}.
 #' @param min_reps the minimum number of replicates in a cell type to retain it.
 #'   Defaults to \code{2}.
 #' @param min_features the minimum number of counts for a gene to retain it.
-#'   Defaults to \code{0}   
+#'   Defaults to \code{0}
 #' @return a cleaned up expression matrix and meta data object
-#'  
+#'
 #' @importFrom dplyr %>% rename_ n_distinct mutate_at vars
 #' @importFrom tester is_numeric_matrix is_numeric_dataframe
 #' @importFrom methods is
-#' 
-check_inputs = function(input, 
-                        meta = NULL, 
+#'
+check_inputs = function(input,
+                        meta = NULL,
                         replicate_col = 'replicate',
                         cell_type_col = 'cell_type',
                         label_col = 'label') {
@@ -45,7 +45,7 @@ check_inputs = function(input,
     if (!is.factor(meta[[label_col]])) {
       labels = meta[[label_col]]
     } else {
-      labels = as.character(meta[[label_col]])  
+      labels = as.character(meta[[label_col]])
     }
     cell_types = as.character(meta[[cell_type_col]])
     expr = Seurat::GetAssayData(input, slot = 'counts')
@@ -63,7 +63,7 @@ check_inputs = function(input,
     if (!is.factor(meta[[label_col]])) {
       labels = meta[[label_col]]
     } else {
-      labels = as.character(meta[[label_col]])  
+      labels = as.character(meta[[label_col]])
     }
     cell_types = as.character(meta[[cell_type_col]])
     expr = monocle3::exprs(input)
@@ -82,7 +82,7 @@ check_inputs = function(input,
     if (!is.factor(meta[[label_col]])) {
       labels = meta[[label_col]]
     } else {
-      labels = as.character(meta[[label_col]])  
+      labels = as.character(meta[[label_col]])
     }
     cell_types = as.character(meta[[cell_type_col]])
     expr = SummarizedExperiment::assay(input)
@@ -94,7 +94,7 @@ check_inputs = function(input,
     if (!valid_input)
       stop("input must be Seurat, monocle, sparse matrix, numeric matrix, or ",
            "numeric data frame")
-    if (is.null(meta)) 
+    if (is.null(meta))
       stop("input matrix must be accompanied by a metadata table")
     expr = input
     if (!is.null(replicate_col))
@@ -102,12 +102,12 @@ check_inputs = function(input,
     labels = as.character(meta[[label_col]])
     cell_types = as.character(meta[[cell_type_col]])
   }
-  
+
   # check dimensions are non-zero
   if (length(dim(expr)) != 2 || !all(dim(expr) > 0)) {
     stop("expression matrix has at least one dimension of size zero")
   }
-  
+
   # check dimensions match
   n_cells1 = nrow(meta)
   n_cells2 = ncol(expr)
@@ -115,12 +115,12 @@ check_inputs = function(input,
     stop("number of cells in metadata (", n_cells1, ") does not match number ",
          "of cells in expression (", n_cells2, ")")
   }
-  
+
   # check at least two labels
   if (n_distinct(labels) == 1) {
     stop("only one label provided: ", unique(labels))
   }
-  
+
   # check for missing labels or cell types
   if (any(is.na(labels))) {
     stop("labels contain ", sum(is.na(labels)), "missing values")
@@ -131,18 +131,18 @@ check_inputs = function(input,
   if (!is.null(replicate_col) && any(is.na(replicates))) {
     stop("replicates contain ", sum(is.na(replicates)), "missing values")
   }
-  
+
   # check for missing replicates
   if (!is.null(replicate_col) && is.null(replicates)) {
     stop("metadata does not contain replicate information")
   }
-  
+
   # remove missing values
   missing = is.na(expr)
   if (any(missing)) {
     stop("matrix contains ", sum(missing), "missing values")
   }
-  
+
   # clean up the meta data
   if (!is.null(replicate_col)) {
     meta %<>%
@@ -158,11 +158,7 @@ check_inputs = function(input,
              label = meta[[label_col]]) %>%
       mutate_at(vars(cell_type, label), as.factor)
   }
-  
-  # make sure we only have 2 labels
-  if (n_distinct(meta$label) != 2) {
-    stop("Please only include 2 distinct experimental labels")
-  }
+
   # make sure meta contains row names and is a data frame
   rownames(meta) = colnames(expr)
   meta = as.data.frame(meta)
